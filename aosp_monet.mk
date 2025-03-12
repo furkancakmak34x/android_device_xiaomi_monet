0cffb41aaf7f60b22e83bcdb201d209e21c6af44
furkan <87311803+furkancakmak34x@users.noreply.github.com>
2025-03-12 11:44:02 +0300

    monet: remove deprecated entrys

--- aosp_monet.mk
+++ aosp_monet.mk
@@ -27,9 +27,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="monet-user 12 SKQ1.211006.001 V13.0.2.0.SJIEUXM release-keys" \
     BuildFingerprint=Xiaomi/monet_eea/monet:12/SKQ1.211006.001/V13.0.2.0.SJIEUXM:user/release-keys
-
-# Inherit from dolby
-$(call inherit-product-if-exists, vendor/sony/dolby/sony-dolby-vendor.mk)
-
-# Inherit from Leica camera
-$(call inherit-product-if-exists, vendor/xiaomi/monet-leicacamera/leica-camera-vendor.mk)
